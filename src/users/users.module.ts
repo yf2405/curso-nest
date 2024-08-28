6a1f205d2be3de6232ec73253f615b90b006425e
YersonFelipe243 <162071436+YersonFelipe243@users.noreply.github.com>
2024-08-28 17:23:06 -0500

    midleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,32 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestMiddleware,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
+//import { AuthMiddleware } from './auth/auth.middleware';
+//import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService]// PrismaService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer
+    //   .apply(LoggerMiddleware)
+    //   .forRoutes(
+    //     { path: '/users', method: RequestMethod.GET },
+    //     {
+    //       path: '/users',
+    //       method: RequestMethod.POST,
+    //     },
+    //   )
+    //   .apply(AuthMiddleware)
+    //   .forRoutes('users');
+  }
+}
